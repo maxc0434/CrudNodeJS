daafa8de38d1d28280d8b8a7cda839f464495bc2
maxc0434 <max.couillet@gmail.com>
2025-09-30 16:10:05 +0200

    maj connex server & premiere route GET

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,27 @@ const corsOptions = {
     credentials: true,
 }
 
+const dataBase = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'crudnode'
+})
+
+app.get("/", (req, res) => {
+  const sql = "SELECT * FROM student";
+  database.query(sql, (err,data) => {
+    if (err) return res.json("Error");
+    return res.json(data);
+  });
+});
+
+
+
+
+
+
+
 app.listen(8081, () => {
   console.log("Server is running on port 8081");
 });
